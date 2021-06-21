7d612f1df81e77b94b7bb70ade9eabec063a70c3
puerdon <dondon726@gmail.com>
2021-06-21 20:27:57 +0800

    update

--- app.py
+++ app.py
@@ -1,19 +1,41 @@
 import os
-from flask import Flask, jsonify
-from gensim.models.wrappers import FastText
+from flask import Flask, jsonify, request
+# from gensim.models.wrappers import FastText
+# from sklearn.metrics.pairwise import cosine_similarity
+from gensim.models.fasttext import load_facebook_model
 
 app = Flask(__name__)
 
 # 載入模型 (大約要 1 分鐘)
-model = FastText.load_fasttext_format('fasttext-cc.zh.300.bin')
+model = load_facebook_model('fasttext-cc.zh.300.bin')
 
 
 
 #we define the route /
 @app.route('/')
 def welcome():
-    # return a json
-    return jsonify({'status': 'api working'})
+    content = request.json
+    # 傳來的 json 格式如下:
+    # 整個 [無聊] 到 [不行]
+    # {target: [無聊, 不行], candidates: [[嗨, 不行], [傻眼, 想死], ...]}
+
+    slot_number = len(a['target'])
+    score_list = []
+
+    for candidate in a['candidates']:
+        sim = 0
+        for i, w in enumerate(candidate):
+    #         print(i)
+    #         print(w)
+            # sim += similarity(a['target'][i], w)
+            sim += model.wv.similarity(a['target'][i], w)
+        sim = sim / slot_number
+        # print(sim)
+        score_list.append(sim)
+    
+    result = [{'score': _, 'candidate': x} for _, x in sorted(zip(score_list, a['candidates']), reverse=True)] 
+
+    return jsonify({'status': 'api working', 'sorted_candidates': result})
 
 if __name__ == '__main__':
     #define the localhost ip and the port that is going to be used
